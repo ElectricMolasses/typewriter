d3e60650feface75bb93480cc70fed0d21e02dfe
Robert Beckett <robert.beckett@onion.red>
2019-10-21 16:38:48 +0000

    Changed to print characters on same line, added newLine to the end to keep term pretty.

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,5 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
 
 for (const char of sentence) {
-  console.log(char);
+  process.stdout.write(char);
 }
